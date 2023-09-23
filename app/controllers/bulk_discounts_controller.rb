61dfce4c1f272cfbb8dc472ffd3d57fae80c777a
Eliza Keating <ek.elizakeating@gmail.com>
2023-09-23 11:12:42 -0600

    feat: add new and create actions to bulk discounts controller

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -7,4 +7,18 @@ class BulkDiscountsController < ApplicationController
   def show
     @bulk_discount = BulkDiscount.find(params[:id])
   end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def create
+    merchant = Merchant.find(params[:merchant_id])
+    merchant.bulk_discounts.create(
+      percentage: params[:percentage],
+      quantity_threshold: params[:quantity_threshold]
+    )
+
+    redirect_to merchant_bulk_discounts_path
+  end
 end
