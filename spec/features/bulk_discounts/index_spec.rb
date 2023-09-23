8578fce9ae2ab6c38a95bdf280225bd2e16588d8
Eliza Keating <ek.elizakeating@gmail.com>
2023-09-23 11:11:26 -0600

    test: add tests for user story 2

--- spec/features/bulk_discounts/index_spec.rb
+++ spec/features/bulk_discounts/index_spec.rb
@@ -6,12 +6,12 @@ RSpec.describe "Merchant Bulk Discounts Index" do
 
     @bulk_discount1 = @merchant1.bulk_discounts.create!(percentage: 5, quantity_threshold: 5)
     @bulk_discount2 = @merchant1.bulk_discounts.create!(percentage: 15, quantity_threshold: 20)
+
+    visit merchant_bulk_discounts_path(@merchant1)
   end
   describe "as a merchant" do
     describe "when I visit my bulk discounts index page" do
       it "I see all of my bulk discounts including their percentage discount and quantity thresholds and each bulk discount listed includes a link to its show page" do
-        visit merchant_bulk_discounts_path(@merchant1)
-
         expect(page).to have_content("Bulk Discounts")
         expect(page).to have_link("5% off orders of 5 or more items")
         expect(page).to have_link("15% off orders of 20 or more items")
@@ -19,6 +19,14 @@ RSpec.describe "Merchant Bulk Discounts Index" do
         click_link("5% off orders of 5 or more items")
 
         expect(current_path).to eq("/merchants/#{@merchant1.id}/bulk_discounts/#{@bulk_discount1.id}")
+      end
+
+      it "I see a link to create a new discount that takes me to a page to create a new discount" do
+        expect(page).to have_link("Create a new discount")
+
+        click_link("Create a new discount")
+        
+        expect(current_path).to eq("/merchants/#{@merchant1.id}/bulk_discounts/new")
       end
     end
   end
---
+++ spec/features/bulk_discounts/new_spec.rb
@@ -0,0 +1,30 @@
+require "rails_helper"
+
+RSpec.describe "New Bulk Discount" do
+  before :each do
+    @merchant1 = Merchant.create!(name: "Hair Care")
+    
+    visit "/merchants/#{@merchant1.id}/bulk_discounts/new"
+  end
+  describe "as a merchant" do
+    describe "when I visit the new bulk discount page" do
+      describe "I see a form to add a new a new bulk discount" do
+        describe "I fill in the form with valid data" do
+          it "I am redirected to the bulk discount index and I see my new bulk discount listed" do
+            expect(page).to have_content("Percentage discount:")
+            expect(page).to have_content("Quantity threshold:")
+
+            fill_in "Percentage discount:", with: "15"
+            fill_in "Quantity threshold:", with: "20"
+
+            click_button "Submit"
+
+            expect(current_path).to eq("/merchants/#{@merchant1.id}/bulk_discounts")
+
+            expect(page).to have_content("15% off orders of 20 or more items")
+          end
+        end
+      end
+    end
+  end
+end
