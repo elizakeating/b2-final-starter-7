ef8458733cb8834ffaa775de05638d8448c20e5e
Eliza Keating <ek.elizakeating@gmail.com>
2023-09-26 14:44:49 -0600

    feat: add tests for new bulk_discoutns associations

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -3,5 +3,6 @@ require "rails_helper"
 RSpec.describe BulkDiscount, type: :model do
   describe "relationships" do
     it { should belong_to :merchant }
+    it { has_many(:invoice_items).through(:merchant) }
   end
 end
