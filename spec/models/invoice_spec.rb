30f7daa919a3d5e0c41a5c8b43d7c51277d72f75
Eliza Keating <ek.elizakeating@gmail.com>
2023-09-25 22:29:43 -0600

    test: add test for discounted_amount and discounted_revenue invoice model methods

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -24,8 +24,42 @@ RSpec.describe Invoice, type: :model do
       expect(@invoice_1.total_revenue).to eq(100)
     end
 
-    it "discounted revenue" do
-      
+    it "discounted_revenue" do
+      merchant_a = Merchant.create(name: "Merchant A")
+
+      item_a = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: merchant_a.id, status: 1)
+      item_b = Item.create!(name: "Conditioner", description: "This makes your hair shiny", unit_price: 8, merchant_id: merchant_a.id, status: 1)
+
+      bulk_discount_1 = merchant_a.bulk_discounts.create(percentage: 20, quantity_threshold: 10)
+      bulk_discount_1 = merchant_a.bulk_discounts.create(percentage: 15, quantity_threshold: 15)
+
+      customer_1 = Customer.create(first_name: "Joey", last_name: "Smith")
+
+      invoice_a = Invoice.create(customer_id: customer_1.id, status: 2)
+
+      invoice_item_1 = InvoiceItem.create!(invoice_id: invoice_a.id, item_id: item_a.id, quantity: 12, unit_price: 253, status: 2)
+      invoice_item_2 = InvoiceItem.create!(invoice_id: invoice_a.id, item_id: item_b.id, quantity: 15, unit_price: 747, status: 2)
+
+      expect(invoice_a.discounted_revenue).to eq(11392.8)
+    end
+
+    it "discounted_amount" do
+      merchant_a = Merchant.create(name: "Merchant A")
+
+      item_a = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: merchant_a.id, status: 1)
+      item_b = Item.create!(name: "Conditioner", description: "This makes your hair shiny", unit_price: 8, merchant_id: merchant_a.id, status: 1)
+
+      bulk_discount_1 = merchant_a.bulk_discounts.create(percentage: 20, quantity_threshold: 10)
+      bulk_discount_1 = merchant_a.bulk_discounts.create(percentage: 15, quantity_threshold: 15)
+
+      customer_1 = Customer.create(first_name: "Joey", last_name: "Smith")
+
+      invoice_a = Invoice.create(customer_id: customer_1.id, status: 2)
+
+      invoice_item_1 = InvoiceItem.create!(invoice_id: invoice_a.id, item_id: item_a.id, quantity: 12, unit_price: 253, status: 2)
+      invoice_item_2 = InvoiceItem.create!(invoice_id: invoice_a.id, item_id: item_b.id, quantity: 15, unit_price: 747, status: 2)
+
+      expect(invoice_a.discounted_amount).to eq(2848.2)
     end
   end
 end
